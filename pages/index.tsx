ef255cce25c4a9811d9f8f20e52e03cc016c6723
gkwmedia <tgusewelle@gkwmedia.com>
2022-06-15 16:41:29 -0500

    added router push index to dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+import { useRouter } from "../node_modules/next/router";
+
 const Index = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/dashboard");
+  }, [router]);
   return <div></div>;
 };
 
